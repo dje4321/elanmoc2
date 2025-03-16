cbf03b9201d6c44a4ebe3dbd3ef0b5b06e8ef374
dje4321 <dje4321@gmail.com>
2025-03-16 02:59:04 -0500

    Added RETRY_TOO_FAST to allow compilation on 1.94.9 libprint targets

--- libfprint/fpi-device.c
+++ libfprint/fpi-device.c
@@ -116,6 +116,9 @@ fpi_device_retry_new (FpDeviceRetry error)
     case FP_DEVICE_RETRY_REMOVE_FINGER:
       msg = "Please try again after removing the finger first.";
       break;
+    case FP_DEVICE_RETRY_TOO_FAST:
+      msg = "The swipe was too fast, please try again.";
+      break;
 
     default:
       g_warning ("Unsupported error, returning general error instead!");
